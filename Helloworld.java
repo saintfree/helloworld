8fb6300b122d7ef72642d7bb3f29020fdb902a5d
saintfree <brian.hoyoung@gmail.com>
2018-09-11 14:16:26 +0900

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 public class Helloworld{
-  public static void main (String arg[]) {
-    System.println.out("Hello world");
+  public static void main (String[] args) {
+    System.out.println("Hello world");
   }
 }
